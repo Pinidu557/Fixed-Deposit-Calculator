f019218604a298dbb6384a088eff0f1f50b80617
Pinidu557 <piniduransika961@gmail.com>
2025-02-15 15:21:15 +0530

    latest update

--- script.js
+++ script.js
@@ -46,28 +46,40 @@ function calculate(e){
       }
     });
 
-    if(ir.value =="" || da.value=="" || month==""|| selectedValue=="" ){
+    if(ir.value == "" || da.value== "" || month==""|| selectedValue=="" ){
       alert("fill the all form filels");
-    }else{
-
+      return;
     }
 
     if(selectedValue == "atmaturit"){
 
-      totAmount = depositamount*Math.pow((1+intersetrate/100/12),(12*month/12));
+      totAmount  = depositamount * Math.pow((1+intersetrate /100/12) , month);      ;
+
+      interset = totAmount-depositamount;
+
+      if(intersetrate == 0){
+        totAmount = 0;
+        interset = 0;
+
+      }
 
       payingtype.textContent = "Interest Paid At Maturity"
       depa.textContent = depositamount.toLocaleString('en-Us',{
         minimumFractionDigits:2,
         maximumFractionDigits:2
       });
-      ti.textContent = "Total Amount (LKR)";
-      interset = totAmount-depositamount
+
+      
+
+      ti.textContent = "Total Amount";
+
       inter.textContent = totAmount.toFixed(2);
 
+      
+
       ta.textContent = interset.toLocaleString('en-US', {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2
+        minimumFractionDigits:2,
+        maximumFractionDigits:2
     });
       
 
@@ -75,7 +87,16 @@ function calculate(e){
 
     if(selectedValue == "monthly" && month>=12){
 
-      totAmount = (depositamount*intersetrate/100/12);
+      totAmount = depositamount*(intersetrate/100/12);
+
+      interset = totAmount*month;
+
+      var totalAmount  = interset+depositamount;
+
+      if(intersetrate == 0){
+        totalAmount = 0;
+        interset = 0;
+      }
 
       ta.textContent = totAmount.toLocaleString('en-Us',{
         minimumFractionDigits: 2,
@@ -84,15 +105,13 @@ function calculate(e){
 
       payingtype.textContent = "Interest Paid Monthly";
       depa.textContent = depositamount.toLocaleString('en-Us',{
-        maximumFractionDigits:2,
-        maximumSignificantDigits : 2
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
       });
 
-      interset = totAmount*month;
-      var totalAmount  = interset+depositamount;
       inter.textContent = totalAmount.toLocaleString('en-US',{
-        minimumFractionDigits:2,
-        maximumFractionDigits:2
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
       });
     }else if(selectedValue == "monthly" && month<12){
       alert("deposit period should be greate than or equal to 12 months");
